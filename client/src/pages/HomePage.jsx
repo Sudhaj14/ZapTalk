aee0c957611a629f15c27adde6beee529261d142
Sudhaj14 <sudha.j2023cse@sece.ac.in>
2025-07-12 08:43:40 +0530

    Fix typo in Sidebar import

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Sidear from '../components/SideBar'
+import SideBar from '../components/SideBar';
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
 import { useContext } from 'react'
